bd439e06564f9f7b53d34a733098ed04ae76f32d
Aaron Schlesinger <aaron@deis.com>
2016-11-01 15:33:32 -0700

    doc(lib/components.go): add godoc to the GetComponents function

--- lib/components.go
+++ lib/components.go
@@ -4,6 +4,8 @@ import (
 	"github.com/deis/steward-framework"
 )
 
+// GetComponents returns a cataloger and lifecycler based on given configuration. Returns
+// nil, nil and a non-nil error if any step in config or component creation failed.
 func GetComponents() (framework.Cataloger, framework.Lifecycler, error) {
 	cfg, err := getConfig()
 	if err != nil {
